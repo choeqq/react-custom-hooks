f53446195141f1ca649354988d9484c34fc8f05e
choeqq <k.chola@op.pl>
2022-05-07 13:13:44 +0200

    useTimeout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ToggleComponent from "./1-useToggle/ToggleComponent";
+import TimeoutComponent from "./2-useTimeout/TimeoutComponent";
 
 function App() {
-  return <ToggleComponent />;
+  // return <ToggleComponent />;
+  return <TimeoutComponent />;
 }
 
 export default App;
