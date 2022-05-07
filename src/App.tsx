ce1cc63e210c06fafb95670cda8e7584b4a04391
choeqq <k.chola@op.pl>
2022-05-07 13:35:51 +0200

    useUpdateEffect, useIsFirstRender add

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ToggleComponent from "./1-useToggle/ToggleComponent";
 import TimeoutComponent from "./2-useTimeout/TimeoutComponent";
 import DebounceComponent from "./3-useDebounce/DebounceComponent";
+import UpdateEffectComponent from "./3-useUpdateEffect/UpdateEffectComponent";
 
 function App() {
   // return <ToggleComponent />;
   // return <TimeoutComponent />;
-  return <DebounceComponent />;
+  // return <DebounceComponent />;
+  return <UpdateEffectComponent />;
 }
 
 export default App;
