7271b77ea2db3b1dff7751357549065bdcfb568b
choeqq <k.chola@op.pl>
2022-05-08 20:37:49 +0200

    useStorage added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import DebounceComponent from "./3-useDebounce/DebounceComponent";
 import UpdateEffectComponent from "./4-useUpdateEffect/UpdateEffectComponent";
 import ArrayComponent from "./5-useArray/ArrayComponent";
 import StateWithHistoryComponent from "./7-useStateWithHistory/StateWithHistory";
+import StorageComponent from "./8-useStorage/StorageComponent";
 
 function App() {
   // return <ToggleComponent />;
@@ -13,7 +14,8 @@ function App() {
   // return <UpdateEffectComponent />;
   // return <ArrayComponent />;
   // return <PreviousComponent />;
-  return <StateWithHistoryComponent />;
+  // return <StateWithHistoryComponent />;
+  return <StorageComponent />;
 }
 
 export default App;
